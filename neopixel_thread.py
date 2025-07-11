200e067f15ba09d2de7ee8b029fde00dd6564b26
Cyborg-Squirrel <ashlybelka@gmail.com>
2025-07-10 22:05:02 -0500

    Broke one method into multiple methods

--- neopixel_thread.py
+++ neopixel_thread.py
@@ -11,16 +11,19 @@ import neopixel_config as npc
 from neopixel_renderer import NeoPixelRenderer
 from rgb_frame import RgbFrame
 
+_logger: logging.Logger
+_renderer: NeoPixelRenderer
 
 def neopixel_thread(queue: mp.Queue, logger: logging.Logger):
     """Starts the thread. This will run in the background until the process is killed."""
-    logger.info("Starting neopixel thread...")
+    _logger = logger
+    _logger.info("Starting neopixel thread...")
     # One hundredth of a second
     queue_timeout_fast = 1 / 100
     # One second
     queue_timeout_slow = 1
     idle = False
-    renderer = NeoPixelRenderer(logger)
+    _renderer = NeoPixelRenderer(_logger)
     while True:
         try:
             queue_msg = queue.get(
@@ -29,38 +32,34 @@ def neopixel_thread(queue: mp.Queue, logger: logging.Logger):
         except Empty:
             queue_msg = None
         if queue_msg is not None and queue_msg.isinstance(npc.NeoPixelConfig):
-            logger.debug("Received NeoPixelConfig " + queue_msg.to_json())
-            validation_result = queue_msg.check_validity()
-            if validation_result.valid:
-                renderer.update_config(queue_msg)
-            else:
-                logger.error("Invalid NeoPixelConfig! " + str(validation_result.reason))
-        elif queue_msg is not None and  queue_msg.isinstance(list):
-            logger.debug("Received NeoPixelConfig list")
-            valid = True
-            reason = ""
-            i = 0
-            while valid and i < len(queue_msg):
-                cfg_validation_result = queue_msg[i].check_validity()
-                valid &= cfg_validation_result.valid
-                if not cfg_validation_result.valid:
-                    reason = cfg_validation_result.reason
-                i += 1
-            if valid:
-                renderer.update_configs(queue_msg)
-            else:
-                logger.error("Invalid config in NeoPixelConfig list! " + str(reason))
+            _logger.debug("Received NeoPixelConfig %s", queue_msg.to_json())
+            _update_config(queue_msg)
+        elif queue_msg is not None and queue_msg.isinstance(list):
+            _logger.debug("Received NeoPixelConfig list")
+            for cfg in queue_msg:
+                _logger.debug("Config %s", queue_msg.to_json())
+                _update_config(cfg)
         elif queue_msg is not None and queue_msg.isinstance(RgbFrame):
-            if queue_msg.options.clear_buffer:
-                renderer.clear_buffer(queue_msg.light_id)
+            _handle_new_frame(queue_msg)
+        idle = _renderer.queue_empty() and queue_msg is None
+        if not _renderer.queue_empty():
+            _renderer.render_queue()
 
-            # If the timestamp is set to 0, render now.
-            # Otherwise queue it to be rendered in the future.
-            if queue_msg.timestamp == 0:
-                renderer.render_frame(queue_msg)
-            else:
-                renderer.queue_frame(queue_msg)
+def _update_config(new_config: npc.NeoPixelConfig):
+    validation_result = new_config.check_validity()
+    if validation_result.valid:
+        _renderer.update_config(new_config)
+    else:
+        _logger.error("Invalid NeoPixelConfig! %s", validation_result.reason)
 
-        idle = renderer.queue_empty() and queue_msg is None
-        if not renderer.queue_empty():
-            renderer.render_queue()
+
+def _handle_new_frame(frame: RgbFrame):
+    if frame.options.clear_buffer:
+        _renderer.clear_buffer(frame.light_id)
+
+    # If the timestamp is set to 0, render now.
+    # Otherwise queue it to be rendered in the future.
+    if frame.timestamp == 0:
+         _renderer.render_frame(frame)
+    else:
+        _renderer.queue_frame(frame)
