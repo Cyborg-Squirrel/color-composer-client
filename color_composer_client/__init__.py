d907ced2adda312eb69a49197df289a2ba62d62f
Cyborg-Squirrel <ashlybelka@gmail.com>
2026-05-04 20:19:17 -0500

    Updated version string to use git ref as part of version string

--- color_composer_client/__init__.py
+++ color_composer_client/__init__.py
@@ -4,7 +4,13 @@ from color_composer_client.neopixel_config import NeoPixelConfig
 from color_composer_client.rgb_frame import RgbFrame, RgbFrameOptions
 from color_composer_client.validation_result import ValidationResult
 
-__version__ = "0.1.0"
+try:
+    import subprocess as _subprocess
+    __version__ = "0.1." + _subprocess.check_output(
+        ["git", "rev-parse", "--short", "HEAD"], stderr=_subprocess.DEVNULL
+    ).decode().strip()
+except Exception:
+    __version__ = "0.1.0"
 
 __all__ = [
     "NeoPixelConfig",
--- color_composer_client/flask_server.py
+++ color_composer_client/flask_server.py
@@ -7,6 +7,7 @@ import multiprocessing as mp
 import queue
 import socket
 import struct
+import subprocess
 from datetime import datetime
 from logging.handlers import RotatingFileHandler
 from typing import Optional
@@ -31,7 +32,13 @@ from color_composer_client.rgb_frame import RgbFrame, RgbFrameOptions
 
 API_PORT = 8000
 WS_PORT = 8765
-VERSION = "0.1.0"
+try:
+    _git_hash = subprocess.check_output(
+        ["git", "rev-parse", "--short", "HEAD"], stderr=subprocess.DEVNULL
+    ).decode().strip()
+    VERSION = f"0.1.{_git_hash}"
+except Exception:
+    VERSION = "0.1.0"
 # maxBytes of a log file is 5MB
 # backupCount number of log files will be created until deleting old log files
 handler = RotatingFileHandler("cc_client.log", maxBytes=5 * 1024 * 1024, backupCount=1)
